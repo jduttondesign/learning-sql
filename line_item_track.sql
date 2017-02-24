6cd8e22a5a18e8252418542e3e35e5df81b79c29
Jennifer Dutton <jenniferdutton@jennifers-mbp.attlocal.net>
2017-02-24 14:17:52 -0600

    first commit

--- line_item_track.sql
+++ line_item_track.sql
@@ -1,2 +1,2 @@
 12) line_item_track.sql: Provide a query that includes the purchased track name with each invoice line item.
-SELECT COUNT(*), InvoiceID FROM InvoiceLIne
+SELECT COUNT(*), InvoiceID FROM InvoiceLine
